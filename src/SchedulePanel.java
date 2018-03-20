afdbd11e24085cecec4809197b72e5501f81ba55
Princedeep Singh <sing0723@algonquinlive.com>
2018-03-19 23:20:25 -0400

    Revert "Ivan"

--- src/SchedulePanel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class SchedulePanel extends JPanel{
-	SchedulePanel(){
-		JLabel text = new JLabel("Schedule Appointment");
-		add(text);
-	}
-}
