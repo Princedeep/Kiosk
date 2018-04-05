fcc98ab85148e7fd1ba3bb7c08b40535f208a9cd
Adam <adam.tremblett7@gmail.com>
2018-04-05 14:09:11 -0400

    Completed majority of Timetable functionality
    Timetable still needs debugging, professor panel and student panel exist only to navigate to different timetables. 
    If other classes are used for left side menu navigation point them to the timetableprof and timetablestudent classes.

---
+++ src/ProfessorPanel.java
@@ -0,0 +1,57 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ProfessorPanel{
+	GridLayout layout = new GridLayout(1, 2);
+	JFrame window = new JFrame("Multiple Panels");
+	JPanel menuPanel = new JPanel(new GridLayout(3,1));
+	JPanel contentPanel = new JPanel();
+	JButton btnTimeTable = new JButton("Timetable");
+	
+	JButton btnNotification = new JButton("Notification");
+	TimetableProf Timetable = new TimetableProf();
+	SchedulePanel ScheduleAppointment = new SchedulePanel();
+	NotificationProf NotificationProf = new NotificationProf();
+		
+	ProfessorPanel(){
+		window.setLayout(layout);
+		
+		menuPanel.add(btnTimeTable);
+		menuPanel.add(btnNotification);
+		
+		window.add(menuPanel);
+		window.add(contentPanel);
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		btnTimeTable.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				window.getContentPane().removeAll();
+				window.repaint();
+				window.add(menuPanel);
+				window.add(Timetable);
+				window.revalidate();
+			}
+		}); 
+		
+		btnNotification.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				window.getContentPane().removeAll();
+				window.repaint();
+				window.add(menuPanel);
+				window.add(NotificationProf);
+				window.revalidate();
+			}
+		});
+	}	
+}
